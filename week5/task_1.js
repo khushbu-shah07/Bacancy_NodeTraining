d83b2c39a770c78f23dd290579e3858a8083acd1
khushbu-shah07 <khushbu.shah@bacancy.com>
2024-02-18 23:46:40 +0530

    week5 tasks added

---
+++ week5/task_1.js
@@ -0,0 +1,23 @@
+"use strict";
+/* Given the data, define the interface "User" and use it accordingly. */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logPerson = exports.users = void 0;
+;
+exports.users = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+function logPerson(user) {
+    console.log(" - ".concat(user.name, ", ").concat(user.age));
+}
+exports.logPerson = logPerson;
+console.log('Users:');
+exports.users.forEach(logPerson);

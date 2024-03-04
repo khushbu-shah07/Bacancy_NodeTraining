3962e555b2a401c72b6404e4e6d17c41bf90a7e9
khushbu-shah07 <khushbu.shah@bacancy.com>
2024-03-04 10:12:18 +0530

    Adds: week5 9 typescript exercises

---
+++ week5/typescript_exercises/task_2.js
@@ -0,0 +1,35 @@
+"use strict";
+/*
+Type "Person" is missing, please define it and use
+    it in persons array and logPerson function in order to fix
+    all the TS errors.
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logPerson = exports.persons = void 0;
+exports.persons = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+function logPerson(user) {
+    console.log(" - ".concat(user.name, ", ").concat(user.age));
+}
+exports.logPerson = logPerson;
+exports.persons.forEach(logPerson);
